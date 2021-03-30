e371519df303f1a5bbf8510011cc25f5c38d79f1
Thies Mueller <git@td00.de>
2021-03-30 20:46:16 +0200

    initial commit, no real code yet

---
+++ inc/header.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<head>
+
+
+<meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <meta name="description" content="">
+     <meta name="author" content="">
+
+
+     <title>QR Schnitzeljagd <?php $_SERVER["SCRIPT_NAME"] ?></title>
+
+
+     <link href="res/css/bootstrap.min.css" rel="stylesheet">
+     <link href="res/css/start.css" rel="stylesheet">
+</head>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+ /*
+ author: Thies Müller
+ contact: contactme@td00.de
+ source: https://github.com/td00/qrschnitzeljagd
+ license: AGPL 3.0
+ */
+include 'inc/db.php';
+include 'inc/header.php';
+?>
