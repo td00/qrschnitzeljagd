deaa8b1590bcc2d9e5c2b25a28ab734ae72f46b8
Thies Mueller <git@td00.de>
2021-03-30 21:33:10 +0200

    lets try smth else

--- qrgen.php
+++ qrgen.php
@@ -1,19 +1,23 @@
 <?php
-    //Include the necessary library for Ubuntu
-    include('/usr/share/phpqrcode/qrlib.php');
-    //Set the data for QR
-    $qrcode = $_GET["data"];
-    $server = $_SERVER["SERVER_NAME"];
-    $code = "https://".$server."/found.php?code=".$qrcode;
-    //check the class is exist or not
-    if(class_exists('QRcode'))
-    {
-        //Generate QR
-        QRcode::png($code);
-    }else{
-        //Print error message
-        echo 'class is not loaded properly';
-    }
+      //Include the necessary library for Ubuntu
+      include('/usr/share/phpqrcode/qrlib.php');
+      $qrcode = $_GET["data"];
+      $server = $_SERVER["SERVER_NAME"];
+      $code = "https://".$server."/found.php?code=".$qrcode;
+      //Set the data for QR
+      $text = $code;
+      //Set the filename with unique id
+      $filename = $qrcode.".png";
+      //Set the error correction Level('L')
+      $e_correction = 'L';
+      //Set pixel size
+      $pixel_size = 12;
+      //Set the frame size
+      $frame_size = 8;
+      //Generates QR image
+      QRcode::png($text, $filename, $e_correction, $pixel_size, $frame_size);
+      //Display the QR image
+      echo "<img src='".$filename."'>";
 
     echo $qrcode;
     echo "<br>";
@@ -22,5 +26,4 @@
     echo $code;
     echo "<br><hr><br>";
 ?>
-<!-- display the QR image -->
-<img src="qrgen.php" />
+
