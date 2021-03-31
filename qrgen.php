ea7c59a953f00acb3d5311d67ccc1a67c4afd795
Thies Mueller <git@td00.de>
2021-03-31 10:22:40 +0200

    pushed the qrcode.js

--- qrgen.php
+++ qrgen.php
@@ -4,6 +4,7 @@
       $code = "https://".$server."/found.php?code=".$qrcode;
 ?>
 <div id="qrcode"></div>
+<script type="text/javascript" src="/res/js/qrcode.js"></script>
 <script type="text/javascript">
 var qrcode = new QRCode(document.getElementById("qrcode"), {
 	text: "<?php echo $code; ?>",
