f68a7be190e8b6be794e4eb22700e991adea3257
Thies Mueller <git@td00.de>
2021-04-01 19:44:21 +0200

    moar print

--- print.php
+++ print.php
@@ -1,5 +1,27 @@
 <?php
 include 'inc/header.php';
 $schnitzel_qrcode = $_GET['code'];
+
 ?>
-<iframe src="qrgen.php?code=<?php echo $schnitzel_qrcode; ?>" />
+<script>
+var body = "dddddd"    
+var script = "<script>window.print();</scr'+'ipt>";
+
+var newWin = $("#printf")[0].contentWindow.document; 
+newWin.open();
+newWin.close();
+
+$("body",newWin).append(body+script);
+
+</script>
+<center>
+<iframe id="printf" src="qrgen.php?code=<?php echo $schnitzel_qrcode; ?>" />
+<br /><br /><br /><hr /><br /><br /><br />
+<button class="btn btn-info" onclick="goBack()">Back</button>
+
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
+</center>
