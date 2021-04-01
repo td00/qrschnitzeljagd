ddd08e1e3a581066db866975aa76a203c11e7d39
Thies Mueller <git@td00.de>
2021-04-01 20:01:10 +0200

    printing like this, maybe? pretty please!

--- print.php
+++ print.php
@@ -5,18 +5,28 @@ $schnitzel_qrcode = $_GET['code'];
 ?>
 <center>
 <script>
-window.onload = function() {
-    var newWin = document.getElementById('printf').contentWindow;
-    newWin.document.write(body);
-    newWin.document.close(); //important!
-    newWin.focus(); //IE fix
-    newWin.print();
-}
-</script>
+        function printDiv() {
+            var divContents = document.getElementById("GFG").innerHTML;
+            var a = window.open('', '', 'height=500, width=500');
+            a.document.write('<html>');
+            a.document.write('<body > ');
+            a.document.write(divContents);
+            a.document.write('</body></html>');
+            a.document.close();
+            a.print();
+        }
+    </script>
+<br>
+<h1 class="h1">Your QR-Code</h1>
 
-<iframe id="printf" src="inc/qrgen.php?code=<?php echo $schnitzel_qrcode; ?>"></iframe>
 
-<br /><br /><br /><hr /><br /><br /><br />
+<br /><br />
+<div id="GFG" style="background-color: green;">
+          
+          <iframe id="printf" src="inc/qrgen.php?code=<?php echo $schnitzel_qrcode; ?>"></iframe>
+        
+      <button class="btn btn-success"><input type="button" value="click" onclick="printDiv()"></button>
+<br /><hr /><br /><br /><br />
 <button class="btn btn-info" onclick="goBack()">Back</button>
 
 <script>
