a5dd68511e4a567f8b85330925810065bdeb8d07
Thies Mueller <git@td00.de>
2021-04-05 17:06:53 +0200

    maybe we can print like this

--- print.php
+++ print.php
@@ -11,10 +11,15 @@ $schnitzel_qrcode = $_GET['code'];
             a.document.write('<html>');
             a.document.write('<body><center> <iframe src="inc/qrgen.php?code=<?php echo $schnitzel_qrcode; ?>"></iframe>');
             a.document.write('<center></body></html>');
-            a.document.close();
+	    a.document.close();
             a.print();
         }
     </script>
+    <script>
+      function printQR() {
+        window.open("print/qr.php?code=<?php echo $schnitzel_qrcode;?>", '', 'height=500, width=500');
+      }
+      </script>
 <br>
 <h1 class="h1">Your QR-Code</h1>
 
@@ -23,8 +28,12 @@ $schnitzel_qrcode = $_GET['code'];
 <div id="GFG" style="">
           <hr />
           <iframe src="inc/qrgen.php?code=<?php echo $schnitzel_qrcode; ?>"></iframe>
-        <br /><br />
-      <input class="btn btn-success" type="button" value="Print" onclick="printDiv()">
+	<br /><br />
+
+<button class="btn btn-success" onclick="printQR()">Print QR Code</button>
+
+<br><br>
+<a href="print/qrtext.php" target="_blank"><button class="btn btn-success">Print QR & Text</button></a><br><input class="btn btn-success" type="button" value="Print" onclick="printDiv()">
 <br /><hr /><br />
 <button class="btn btn-info" onclick="goBack()">Back</button>
 
---
+++ print/qr.php
@@ -0,0 +1,12 @@
+<?php
+include '/inc/header.php';
+$schnitzel_qrcode = $_GET['code'];
+
+
+?>
+<html>
+<body><center> <iframe src="/inc/qrgen.php?code=<?php echo $schnitzel_qrcode; ?>"></iframe>
+<script>
+print();
+</script>
+</center></body></html>
